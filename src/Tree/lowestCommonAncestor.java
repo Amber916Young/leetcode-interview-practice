a9138771a61c94654650495dcb572835ebc2f779
Amber916Young <youngyejia@163.com>
2022-10-20 17:04:01 +0100

    Lowest Common Ancestor of BST

--- src/Tree/lowestCommonAncestor.java
+++ src/Tree/lowestCommonAncestor.java
@@ -1,5 +1,6 @@
 package Tree;
 
+
 /**
  * @ClassName:lowestCommonAncestor
  * @Auther: yyj
@@ -8,4 +9,17 @@ package Tree;
  * @Version: v1.0
  */
 public class lowestCommonAncestor {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if( root == null) return null;
+        if(root == p || root == q) return root;
+
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+
+        if(left != null && right != null) return root;
+        if(left == null && right == null) return null;
+
+        return left != null ? left : right;
+    }
 }
